9bf0075af5aaf752f0b9ffd7d9bbaf713093f105
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-22 19:43:06 +0530

    Update Day22problem2.c

--- Day22problem2.c
+++ Day22problem2.c
@@ -2,7 +2,6 @@ struct ListNode* reverseList(struct ListNode* head) {
     struct ListNode *prev = NULL;
     struct ListNode *current = head;
     struct ListNode *nextNode = NULL;
-
     while (current != NULL) {
         nextNode = current->next;   
         current->next = prev;      
