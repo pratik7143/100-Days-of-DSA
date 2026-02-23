1fba18594cbe317156428a94ecbe82d9010c7832
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-23 18:39:38 +0530

    Update Day23problem2.c

--- Day23problem2.c
+++ Day23problem2.c
@@ -3,7 +3,6 @@ bool hasCycle(struct ListNode *head) {
         return false;
     struct ListNode *slow = head;
     struct ListNode *fast = head;
-
     while (fast != NULL && fast->next != NULL) {
         slow = slow->next;           
         fast = fast->next->next;    
