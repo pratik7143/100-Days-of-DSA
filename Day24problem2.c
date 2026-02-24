99d977ac56462fdcd8f5cb36baa4de6e0fd147fe
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-24 23:18:06 +0530

    Create Day24problem2.c

---
+++ Day24problem2.c
@@ -0,0 +1,23 @@
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Remove leading nodes with target value
+    while (head != NULL && head->val == val) {
+        struct ListNode* temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    if (head == NULL) return head;
+
+    struct ListNode* curr = head;
+    while (curr->next != NULL) {
+        if (curr->next->val == val) {
+            struct ListNode* temp = curr->next;
+            curr->next = curr->next->next;
+            free(temp);
+        } else {
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
