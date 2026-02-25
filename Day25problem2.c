dfbf319e3e4a86d83a2e04a7d7752b604ca18c9d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-25 18:07:06 +0530

    Create Day25problem2.c

---
+++ Day25problem2.c
@@ -0,0 +1,23 @@
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *slow = head, *fast = head;
+
+   
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)  
+            break;
+    }
+
+    if (fast == NULL || fast->next == NULL)
+        return NULL;
+
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow; 
+}
