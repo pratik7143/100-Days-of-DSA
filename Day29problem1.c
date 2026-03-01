af6ca6a585294348a5edb9e816586244e5ecfc31
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-01 22:24:21 +0530

    Create Day29problem1.c

---
+++ Day29problem1.c
@@ -0,0 +1,52 @@
+#include <stdlib.h> // For malloc
+
+// Definition for singly-linked list is already provided in your problem statement
+// struct ListNode {
+//     int val;
+//     struct ListNode *next;
+// };
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    // We create a dummy node on the stack. The actual result list will
+    // start at dummy.next. This simplifies the creation of the list head.
+    struct ListNode dummy; 
+    dummy.next = NULL;
+    
+    // 'curr' pointer is used to build the new list
+    struct ListNode* curr = &dummy;
+    
+    int carry = 0;
+    
+    // Loop continues as long as there are digits left in l1 or l2, 
+    // OR if there is a leftover carry that needs to be added as a new node.
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+        
+        // Add l1's value if it's available
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        
+        // Add l2's value if it's available
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        // Calculate the new carry for the next decimal place (could be 1 or 0)
+        carry = sum / 10;
+        
+        // Allocate a new node for the resulted digit
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10; // Get the digit to store (0-9)
+        newNode->next = NULL;
+        
+        // Append it to our result list and move the 'curr' pointer forward
+        curr->next = newNode;
+        curr = curr->next;
+    }
+    
+    // The head of the resulting numeric list is the node after the dummy
+    return dummy.next;
+}
