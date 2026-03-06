2cc3bec5f13f987b6cc222c9629913ed4baa3170
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-06 20:48:08 +0530

    Create Day34problem2.c

---
+++ Day34problem2.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+
+int calculate(char* s) {
+    long result = 0;
+    long lastNum = 0;
+    long num = 0;
+    char op = '+';
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        
+        if (isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+        }
+
+        if ((!isdigit(s[i]) && s[i] != ' ') || s[i+1] == '\0') {
+
+            if (op == '+') {
+                result += lastNum;
+                lastNum = num;
+            }
+            else if (op == '-') {
+                result += lastNum;
+                lastNum = -num;
+            }
+            else if (op == '*') {
+                lastNum = lastNum * num;
+            }
+            else if (op == '/') {
+                lastNum = lastNum / num;
+            }
+
+            op = s[i];
+            num = 0;
+        }
+    }
+
+    return result + lastNum;
+}
