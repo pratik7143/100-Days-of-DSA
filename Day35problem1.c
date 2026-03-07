f13284caa05beffed044b20ec648834380c7c9f3
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-07 22:23:51 +0530

    Create Day35problem1.c

---
+++ Day35problem1.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int in[100];
+    int out[100];
+    int topIn;
+    int topOut;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+    MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+    obj->topIn = -1;
+    obj->topOut = -1;
+    return obj;
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    obj->in[++obj->topIn] = x;
+}
+
+int myQueuePop(MyQueue* obj) {
+
+    if(obj->topOut == -1){
+        while(obj->topIn != -1){
+            obj->out[++obj->topOut] = obj->in[obj->topIn--];
+        }
+    }
+
+    return obj->out[obj->topOut--];
+}
+
+int myQueuePeek(MyQueue* obj) {
+
+    if(obj->topOut == -1){
+        while(obj->topIn != -1){
+            obj->out[++obj->topOut] = obj->in[obj->topIn--];
+        }
+    }
+
+    return obj->out[obj->topOut];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return obj->topIn == -1 && obj->topOut == -1;
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj);
+}
