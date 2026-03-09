785a7502020b0f0ca56fccab35978dd0dbfb9f35
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-09 21:10:05 +0530

    Update Day37problem2.c

--- Day37problem2.c
+++ Day37problem2.c
@@ -3,39 +3,30 @@
 typedef struct {
     int *heap;
     int size;
-    int capacity;
     int k;
 } KthLargest;
 
-void swap(int *a, int *b)
-{
+void swap(int *a, int *b){
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-void heapifyUp(KthLargest* obj, int index)
-{
-    while(index > 0)
-    {
-        int parent = (index - 1) / 2;
-        if(obj->heap[parent] > obj->heap[index])
-        {
-            swap(&obj->heap[parent], &obj->heap[index]);
-            index = parent;
-        }
-        else
-            break;
+void heapifyUp(KthLargest* obj, int i){
+    while(i > 0){
+        int parent = (i - 1) / 2;
+        if(obj->heap[parent] > obj->heap[i]){
+            swap(&obj->heap[parent], &obj->heap[i]);
+            i = parent;
+        } else break;
     }
 }
 
-void heapifyDown(KthLargest* obj, int index)
-{
-    while(1)
-    {
-        int smallest = index;
-        int left = 2*index + 1;
-        int right = 2*index + 2;
+void heapifyDown(KthLargest* obj, int i){
+    while(1){
+        int smallest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
 
         if(left < obj->size && obj->heap[left] < obj->heap[smallest])
             smallest = left;
@@ -43,60 +34,52 @@ void heapifyDown(KthLargest* obj, int index)
         if(right < obj->size && obj->heap[right] < obj->heap[smallest])
             smallest = right;
 
-        if(smallest != index)
-        {
-            swap(&obj->heap[index], &obj->heap[smallest]);
-            index = smallest;
-        }
-        else
-            break;
+        if(smallest != i){
+            swap(&obj->heap[i], &obj->heap[smallest]);
+            i = smallest;
+        } else break;
     }
 }
 
-void insert(KthLargest* obj, int val)
-{
+void insert(KthLargest* obj, int val){
     obj->heap[obj->size] = val;
     obj->size++;
     heapifyUp(obj, obj->size - 1);
 }
 
-void removeMin(KthLargest* obj)
-{
-    obj->heap[0] = obj->heap[obj->size - 1];
-    obj->size--;
-    heapifyDown(obj, 0);
-}
-
-KthLargest* kthLargestCreate(int k, int* nums, int numsSize)
-{
+KthLargest* kthLargestCreate(int k, int* nums, int numsSize) {
     KthLargest* obj = (KthLargest*)malloc(sizeof(KthLargest));
-    obj->heap = (int*)malloc(sizeof(int) * (k + numsSize));
+    obj->heap = (int*)malloc(sizeof(int) * k);
     obj->size = 0;
-    obj->capacity = k + numsSize;
     obj->k = k;
 
-    for(int i = 0; i < numsSize; i++)
-    {
-        insert(obj, nums[i]);
-        if(obj->size > k)
-            removeMin(obj);
+    for(int i = 0; i < numsSize; i++){
+        if(obj->size < k){
+            insert(obj, nums[i]);
+        }
+        else if(nums[i] > obj->heap[0]){
+            obj->heap[0] = nums[i];
+            heapifyDown(obj, 0);
+        }
     }
 
     return obj;
 }
 
-int kthLargestAdd(KthLargest* obj, int val)
-{
-    insert(obj, val);
+int kthLargestAdd(KthLargest* obj, int val) {
 
-    if(obj->size > obj->k)
-        removeMin(obj);
+    if(obj->size < obj->k){
+        insert(obj, val);
+    }
+    else if(val > obj->heap[0]){
+        obj->heap[0] = val;
+        heapifyDown(obj, 0);
+    }
 
     return obj->heap[0];
 }
 
-void kthLargestFree(KthLargest* obj)
-{
+void kthLargestFree(KthLargest* obj) {
     free(obj->heap);
     free(obj);
 }
