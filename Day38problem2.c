e68138e512e59844389f975b02d6f35e80556c4f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-10 23:13:24 +0530

    Update Day38problem2.c

--- Day38problem2.c
+++ Day38problem2.c
@@ -2,7 +2,6 @@
 int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
     int *result = (int*)malloc((numsSize - k + 1) * sizeof(int));
     int *deque = (int*)malloc(numsSize * sizeof(int)); // store indices
-    
     int front = 0, rear = -1;
     int idx = 0;
     
