689b2e1d875ffa08cfb8a1f0ff8a74887d381fe0
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-10 23:13:47 +0530

    Update Day38problem2.c

--- Day38problem2.c
+++ Day38problem2.c
@@ -9,11 +9,9 @@ int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
         // Remove elements outside window
         if(front <= rear && deque[front] <= i - k)
             front++;
-        
         // Remove smaller elements from rear
         while(front <= rear && nums[deque[rear]] < nums[i])
             rear--;
-        
         // Add current index
         deque[++rear] = i;
         
