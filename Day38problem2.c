1b99a5f8507e6e0114f236f54d6823b5e88bbcc8
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-10 23:12:45 +0530

    Create Day38problem2.c

---
+++ Day38problem2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
+    
+    int *result = (int*)malloc((numsSize - k + 1) * sizeof(int));
+    int *deque = (int*)malloc(numsSize * sizeof(int)); // store indices
+    
+    int front = 0, rear = -1;
+    int idx = 0;
+    
+    for(int i = 0; i < numsSize; i++)
+    {
+        // Remove elements outside window
+        if(front <= rear && deque[front] <= i - k)
+            front++;
+        
+        // Remove smaller elements from rear
+        while(front <= rear && nums[deque[rear]] < nums[i])
+            rear--;
+        
+        // Add current index
+        deque[++rear] = i;
+        
+        // Store result when first window is complete
+        if(i >= k - 1)
+            result[idx++] = nums[deque[front]];
+    }
+    
+    *returnSize = numsSize - k + 1;
+    
+    free(deque);
+    return result;
+}
