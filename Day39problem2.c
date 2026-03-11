ce49cd6dc842c42f13827badc12fe65078a21ef3
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-11 21:59:46 +0530

    Update Day39problem2.c

--- Day39problem2.c
+++ Day39problem2.c
@@ -14,7 +14,6 @@ int* topKFrequent(int* nums, int numsSize, int k, int* returnSize)
 {
     Pair arr[numsSize];
     int size = 0;
-
     for(int i = 0; i < numsSize; i++)
     {
         int found = 0;
