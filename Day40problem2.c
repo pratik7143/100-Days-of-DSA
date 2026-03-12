b7e810275e500854dea5584ee3aa6c38b519074a
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-12 23:22:00 +0530

    Update Day40problem2.c

--- Day40problem2.c
+++ Day40problem2.c
@@ -2,7 +2,6 @@
 int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
     int* result = (int*)calloc(temperaturesSize, sizeof(int));
     int* stack = (int*)malloc(sizeof(int) * temperaturesSize);
-    
     int top = -1;
     
     for(int i = 0; i < temperaturesSize; i++)
