144448eed105df322ea909527899256f34d9746b
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-12 23:21:24 +0530

    Create Day40problem2.c

---
+++ Day40problem2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    
+    int* result = (int*)calloc(temperaturesSize, sizeof(int));
+    int* stack = (int*)malloc(sizeof(int) * temperaturesSize);
+    
+    int top = -1;
+    
+    for(int i = 0; i < temperaturesSize; i++)
+    {
+        while(top != -1 && temperatures[i] > temperatures[stack[top]])
+        {
+            int idx = stack[top--];
+            result[idx] = i - idx;
+        }
+        
+        stack[++top] = i;
+    }
+    
+    free(stack);
+    
+    *returnSize = temperaturesSize;
+    return result;
+}
