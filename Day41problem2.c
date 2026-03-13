7614b930cecb3b53bdca1709c36ce656eaf7979f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-13 23:31:17 +0530

    Update Day41problem2.c

--- Day41problem2.c
+++ Day41problem2.c
@@ -13,7 +13,6 @@ int leastInterval(char* tasks, int tasksSize, int n) {
         if(freq[i] == maxFreq)
             maxCount++;
     }
-    
     int intervals = (maxFreq - 1) * (n + 1) + maxCount;
     
     return tasksSize > intervals ? tasksSize : intervals;
