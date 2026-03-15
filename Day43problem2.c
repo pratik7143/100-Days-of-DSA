22fece891924f7506cc47a32460761f246ef1a06
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-15 23:20:59 +0530

    Create Day43problem2.c

---
+++ Day43problem2.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+void inorder(struct TreeNode* root, int* arr, int* index){
+    if(root == NULL)
+        return;
+
+    inorder(root->left, arr, index);   // Left
+    arr[(*index)++] = root->val;       // Root
+    inorder(root->right, arr, index);  // Right
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    
+    int* arr = (int*)malloc(100 * sizeof(int)); // allocate memory
+    int index = 0;
+
+    inorder(root, arr, &index);
+
+    *returnSize = index;
+    return arr;
+}
