9e359f18504be8090a7323ef77034061c03d6096
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-17 22:43:51 +0530

    Update Day45problem2.c

--- Day45problem2.c
+++ Day45problem2.c
@@ -2,7 +2,6 @@ int maxDepth(struct TreeNode* root) {
     if (root == NULL) {
         return 0;
     }
-    
     int leftDepth = maxDepth(root->left);
     int rightDepth = maxDepth(root->right);
     
