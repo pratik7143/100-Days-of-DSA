5b81fe5e81b59e506f9da4b9dd960ccbcaa8fcaa
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-18 22:37:02 +0530

    Update Day46problem2.c

--- Day46problem2.c
+++ Day46problem2.c
@@ -42,7 +42,6 @@ int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes
     *returnColumnSizes = (int*)malloc(sizeof(int) * capacity);
     struct Queue* q = createQueue(capacity);
     enqueue(q, root);
-
     int levelCount = 0;
 
     while (!isEmpty(q)) {
