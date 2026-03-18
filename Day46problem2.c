968be2daf5ce338119a8c4a8e6949d7c14697c11
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-18 22:35:02 +0530

    Update Day46problem2.c

--- Day46problem2.c
+++ Day46problem2.c
@@ -21,7 +21,6 @@ struct Queue* createQueue(int capacity) {
     q->size = 0;
     return q;
 }
-
 void enqueue(struct Queue* q, struct TreeNode* node) {
     q->arr[q->rear++] = node;
     q->size++;
