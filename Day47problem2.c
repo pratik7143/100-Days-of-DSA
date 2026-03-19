663d5ba6ee6b29dfbad99ab1f17dd985b76ac5db
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-19 19:36:23 +0530

    Create Day47problem2.c

---
+++ Day47problem2.c
@@ -0,0 +1,23 @@
+int maxDepth(struct TreeNode* root) {
+    if (!root) return 0;
+
+    struct TreeNode* queue[10000];
+    int front = 0, rear = 0;
+    
+    queue[rear++] = root;
+    int depth = 0;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* node = queue[front++];
+            
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+        depth++;
+    }
+
+    return depth;
+}
