58b437d32e2be405551889070d5f2880f368d439
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-20 18:17:29 +0530

    Update Day48problem2.c

--- Day48problem2.c
+++ Day48problem2.c
@@ -7,7 +7,8 @@ int sumOfLeftLeaves(struct TreeNode* root)
     int sum = 0;
     if (root->left != NULL && 
         root->left->left == NULL && 
-        root->left->right == NULL) {
+        root->left->right == NULL) 
+    {
         sum += root->left->val;
     }
     sum += sumOfLeftLeaves(root->left);
