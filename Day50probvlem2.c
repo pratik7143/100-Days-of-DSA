b7fa1d7348d9a3e82e822c232b0404eae62f02e1
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-22 23:11:49 +0530

    Update Day50probvlem2.c

--- Day50probvlem2.c
+++ Day50probvlem2.c
@@ -3,7 +3,8 @@ struct TreeNode* searchBST(struct TreeNode* root, int val)
     if (root == NULL || root->val == val) {
         return root;
     }
-    if (val < root->val) {
+    if (val < root->val) 
+    {
         return searchBST(root->left, val);
     } else {
         return searchBST(root->right, val);
