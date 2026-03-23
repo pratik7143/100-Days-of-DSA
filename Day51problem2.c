28e0285d3800e72f4ac9dd7520e792ac82ab4355
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-23 21:56:13 +0530

    Update Day51problem2.c

--- Day51problem2.c
+++ Day51problem2.c
@@ -1,6 +1,7 @@
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) 
 {
-    while (root != NULL) {
+    while (root != NULL) 
+    {
         if (p->val < root->val && q->val < root->val) 
         {
             root = root->left;
