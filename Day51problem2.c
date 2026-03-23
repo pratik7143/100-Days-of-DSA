c8c6e759d0b5fddcdeda99fc9237cb80b3bb3490
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-23 21:53:40 +0530

    Create Day51problem2.c

---
+++ Day51problem2.c
@@ -0,0 +1,17 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        // If both nodes are smaller, go left
+        if (p->val < root->val && q->val < root->val) {
+            root = root->left;
+        }
+        // If both nodes are greater, go right
+        else if (p->val > root->val && q->val > root->val) {
+            root = root->right;
+        }
+        // Otherwise, this is the split point (LCA)
+        else {
+            return root;
+        }
+    }
+    return NULL;
+}
