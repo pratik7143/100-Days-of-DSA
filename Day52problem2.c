8ba7db9aa73cbc77a24aa1932907ff8c4a45ca15
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-24 22:34:25 +0530

    Update Day52problem2.c

--- Day52problem2.c
+++ Day52problem2.c
@@ -3,7 +3,7 @@ struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p,
     if (root == p || root == q) return root;
     struct TreeNode* left = lowestCommonAncestor(root->left, p, q);
     struct TreeNode* right = lowestCommonAncestor(root->right, p, q);
-    // if both sides return non-null → this is LCA
+    
     if (left != NULL && right != NULL) return root;
 
     // otherwise return the non-null side
