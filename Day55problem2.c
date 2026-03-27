3daa59afb7420899025d80be9eeb739c1ebc956d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-27 19:27:57 +0530

    Create Day55problem2.c

---
+++ Day55problem2.c
@@ -0,0 +1,32 @@
+int* rightSideView(struct TreeNode* root, int* returnSize) {
+    if (root == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(1000 * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+
+    int* result = (int*)malloc(1000 * sizeof(int));
+    *returnSize = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int levelSize = rear - front;
+
+        for (int i = 0; i < levelSize; i++) {
+            struct TreeNode* node = queue[front++];
+
+            if (i == levelSize - 1) {
+                result[*returnSize] = node->val;
+                (*returnSize)++;
+            }
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+    }
+
+    return result;
+}
