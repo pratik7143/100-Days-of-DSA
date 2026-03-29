ee41fd49028f403c68014ea831f61a5448ff90cd
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-29 21:55:00 +0530

    Update Day57problem2.c

--- Day57problem2.c
+++ Day57problem2.c
@@ -2,7 +2,6 @@ void flatten(struct TreeNode* root) {
     struct TreeNode* curr = root;
     while (curr != NULL) {
         if (curr->left != NULL) {
-            
             struct TreeNode* prev = curr->left;
             while (prev->right != NULL) {
                 prev = prev->right;
