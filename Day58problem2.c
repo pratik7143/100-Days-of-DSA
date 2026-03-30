304610881abd5d05884b9da981eb428c288304d4
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-30 16:16:10 +0530

    Create Day58problem2.c

---
+++ Day58problem2.c
@@ -0,0 +1,34 @@
+// Helper function
+int findIndex(int* inorder, int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct TreeNode* build(
+    int* preorder, int preStart, int preEnd,
+    int* inorder, int inStart, int inEnd
+) {
+    if (preStart > preEnd || inStart > inEnd)
+        return NULL;
+
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = preorder[preStart];
+
+    int inIndex = findIndex(inorder, inStart, inEnd, root->val);
+    int leftSize = inIndex - inStart;
+
+    root->left = build(preorder, preStart + 1, preStart + leftSize,
+                       inorder, inStart, inIndex - 1);
+
+    root->right = build(preorder, preStart + leftSize + 1, preEnd,
+                        inorder, inIndex + 1, inEnd);
+
+    return root;
+}
+
+struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
+    return build(preorder, 0, preorderSize - 1, inorder, 0, inorderSize - 1);
+}
