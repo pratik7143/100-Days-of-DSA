62a7f98864a4730eeedc91ee8d0e0653f42c11aa
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-03-31 19:11:34 +0530

    Update Day59problem2.c

--- Day59problem2.c
+++ Day59problem2.c
@@ -8,7 +8,6 @@ int findIndex(int* inorder, int start, int end, int val) {
 }
 struct TreeNode* build(int* inorder, int inStart, int inEnd,
                        int* postorder, int* postIndex) {
-    
     if (inStart > inEnd)
         return NULL;
 
