0de685914fc31e525126bc29f6f1a6b39b2ccc39
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-02 21:32:22 +0530

    Update Day61problem2.c

--- Day61problem2.c
+++ Day61problem2.c
@@ -8,7 +8,6 @@ void dfs(int** isConnected, int n, int city, bool* visited) {
         }
     }
 }
-
 int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
     int n = isConnectedSize;
     bool visited[n];
