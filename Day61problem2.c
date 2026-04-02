f2f847b5086be2495eae2bcd58116f2fd8b5e583
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-02 21:32:15 +0530

    Update Day61problem2.c

--- Day61problem2.c
+++ Day61problem2.c
@@ -2,7 +2,6 @@
 // DFS function
 void dfs(int** isConnected, int n, int city, bool* visited) {
     visited[city] = true;
-
     for (int i = 0; i < n; i++) {
         if (isConnected[city][i] == 1 && !visited[i]) {
             dfs(isConnected, n, i, visited);
