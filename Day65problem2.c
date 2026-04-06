e0f40d0f53e3fa45bffe95d2362024a89306e231
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-06 19:58:21 +0530

    Update Day65problem2.c

--- Day65problem2.c
+++ Day65problem2.c
@@ -17,7 +17,6 @@ bool unionSet(int parent[], int u, int v) {
 }
 bool isCycle(int edges[][2], int E, int V) {
     int* parent = (int*)malloc(V * sizeof(int));
-
     // Initialize
     for (int i = 0; i < V; i++)
         parent[i] = i;
