80ea2ad5a94fa17c44035fc8747820ea70e5e254
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-06 19:57:22 +0530

    Create Day65problem2.c

---
+++ Day65problem2.c
@@ -0,0 +1,40 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Find with path compression
+int find(int parent[], int x) {
+    if (parent[x] != x)
+        parent[x] = find(parent, parent[x]);
+    return parent[x];
+}
+
+// Union function
+bool unionSet(int parent[], int u, int v) {
+    int pu = find(parent, u);
+    int pv = find(parent, v);
+
+    if (pu == pv)
+        return true; // cycle found
+
+    parent[pu] = pv;
+    return false;
+}
+
+bool isCycle(int edges[][2], int E, int V) {
+    int* parent = (int*)malloc(V * sizeof(int));
+
+    // Initialize
+    for (int i = 0; i < V; i++)
+        parent[i] = i;
+
+    // Process edges
+    for (int i = 0; i < E; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        if (unionSet(parent, u, v))
+            return true;
+    }
+
+    return false;
+}
