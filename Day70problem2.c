ad1a3bac027d8d59bbbe343129da89d23fe712bc
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-12 22:24:55 +0530

    Update Day70problem2.c

--- Day70problem2.c
+++ Day70problem2.c
@@ -14,7 +14,6 @@ int findCheapestPrice(int n, int** flights, int flightsSize, int* flightsColSize
             int u = flights[j][0];
             int v = flights[j][1];
             int price = flights[j][2];
-            // If the source of the flight is reachable
             if (dist[u] != INT_MAX) {
                 if (dist[u] + price < temp[v]) {
                     temp[v] = dist[u] + price;
