27e154e4d36ce9d8b1ef97187807e2fbfdf1a63c
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-15 21:12:16 +0530

    Create Day74problem2.c

---
+++ Day74problem2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// DFS function
+void dfs(int** isConnected, int n, int city, int* visited) {
+    visited[city] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (isConnected[city][i] == 1 && !visited[i]) {
+            dfs(isConnected, n, i, visited);
+        }
+    }
+}
+
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int n = isConnectedSize;
+    int visited[n];
+    
+    // Initialize visited array
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    int provinces = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(isConnected, n, i, visited);
+            provinces++;  // New province found
+        }
+    }
+
+    return provinces;
+}
