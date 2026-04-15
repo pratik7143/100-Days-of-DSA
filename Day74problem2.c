bb34eba7cd9a285a5b736d01c3c6002d31b7da8f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-15 21:13:07 +0530

    Update Day74problem2.c

--- Day74problem2.c
+++ Day74problem2.c
@@ -10,7 +10,6 @@ void dfs(int** isConnected, int n, int city, int* visited) {
 int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
     int n = isConnectedSize;
     int visited[n];
-    
     for (int i = 0; i < n; i++) {
         visited[i] = 0;
     }
