f7129c334381ba3d0b50897779629e82bfda1086
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-25 23:19:10 +0530

    Update Day80problem2.c

--- Day80problem2.c
+++ Day80problem2.c
@@ -17,7 +17,8 @@ int findTheCity(int n, int** edges, int edgesSize, int* edgesColSize, int distan
         dist[v][u] = w;
     }
     // Step 3: Floyd-Warshall
-    for (int k = 0; k < n; k++) {
+    for (int k = 0; k < n; k++) 
+    {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX) 
