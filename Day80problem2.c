99cd5ed4552b008c86affd54ac48b871514d0f44
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-25 23:18:56 +0530

    Update Day80problem2.c

--- Day80problem2.c
+++ Day80problem2.c
@@ -32,7 +32,8 @@ int findTheCity(int n, int** edges, int edgesSize, int* edgesColSize, int distan
     }
     int minReachable = INT_MAX;
     int resultCity = -1;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         int count = 0;
         for (int j = 0; j < n; j++) {
             if (i != j && dist[i][j] <= distanceThreshold) {
