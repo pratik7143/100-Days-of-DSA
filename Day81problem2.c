49653221859e1d78b908be0cffe21c5300ce1986
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-27 23:17:51 +0530

    Create Day81problem2.c

---
+++ Day81problem2.c
@@ -0,0 +1,27 @@
+int search(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        // Left half sorted
+        if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        // Right half sorted
+        else {
+            if (target > nums[mid] && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+
+    return -1;
+}
