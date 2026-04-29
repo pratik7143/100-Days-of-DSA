b6a15359d672350848fe05b45851d751643c0f3f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-29 23:37:25 +0530

    Update Day81problem2.c

--- Day81problem2.c
+++ Day81problem2.c
@@ -1,7 +1,8 @@
 int search(int* nums, int numsSize, int target) 
 {
     int low = 0, high = numsSize - 1;
-    while (low <= high) {
+    while (low <= high) 
+    {
         int mid = (low + high) / 2;
         if (nums[mid] == target)
             return mid;
