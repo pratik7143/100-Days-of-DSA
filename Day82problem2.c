a36f83c63fe96aa88d4b6578cade25fb1c89b9fe
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-04-30 23:46:20 +0530

    Update Day82problem2.c

--- Day82problem2.c
+++ Day82problem2.c
@@ -3,7 +3,7 @@ int searchInsert(int* nums,int numsSize,int target)
     int low = 0, high = numsSize-1;
     while (low <= high)
         {
-        int mid = (low + high) / 2;
+        int mid = (low + high) /2;
         if (nums[mid] == target)
             return mid;
         else if (nums[mid] < target)
