1a4a171538df6dc7581d10af2d8d156fcdda9266
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-05-01 21:55:38 +0530

    Create Day85problem2.c

---
+++ Day85problem2.c
@@ -0,0 +1,17 @@
+int findMin(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[right]) {
+            // Minimum is in right half
+            left = mid + 1;
+        } else {
+            // Minimum is in left half including mid
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
