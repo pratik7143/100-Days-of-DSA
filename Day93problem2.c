2ae3b59ca62f704f77069da44832d12051483f27
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-05-06 23:33:13 +0530

    Create Day93problem2.c

---
+++ Day93problem2.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* insertionSortList(struct ListNode* head) {
+    
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    struct ListNode dummy;
+    dummy.next = NULL;
+
+    struct ListNode *curr = head;
+
+    while(curr != NULL) {
+        
+        struct ListNode *nextNode = curr->next;
+
+        // Find correct position
+        struct ListNode *prev = &dummy;
+
+        while(prev->next != NULL && prev->next->val < curr->val) {
+            prev = prev->next;
+        }
+
+        // Insert node
+        curr->next = prev->next;
+        prev->next = curr;
+
+        curr = nextNode;
+    }
+
+    return dummy.next;
+}
